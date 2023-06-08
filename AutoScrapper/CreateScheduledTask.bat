e2d16703550d2142a1480e7cb45faa0cf0ab27c4
sohweiyu2023 <134089475+sohweiyu2023@users.noreply.github.com>
2023-06-08 20:30:09 +0800

    Update CreateScheduledTask.bat

--- AutoScrapper/CreateScheduledTask.bat
+++ AutoScrapper/CreateScheduledTask.bat
@@ -1,2 +1,3 @@
 @echo off
 schtasks /Create /SC WEEKLY /D THU /TN "ScrapeAppTask" /TR C:\Users\cyber\source\repos\AutoScrapper\RunAutoScrapper.bat /RL HIGHEST /ST 09:00 /F
+powershell -Command "Set-ScheduledTask -TaskName 'ScrapeAppTask' -Settings (New-ScheduledTaskSettingsSet -StartWhenAvailable)"
